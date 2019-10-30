91212e74eb8aad2fb5c96e2c3fcd45e5e1cb3929
Will Harvey <wsgh@pop-os.localdomain>
2019-10-30 15:25:24 -0700

    reduced L1 loss weight

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -32,7 +32,7 @@ class Pix2PixModel(BaseModel):
         parser.set_defaults(norm='batch', netG='unet_256', dataset_mode='aligned')
         if is_train:
             parser.set_defaults(pool_size=0, gan_mode='vanilla')
-            parser.add_argument('--lambda_L1', type=float, default=100.0, help='weight for L1 loss')
+            parser.add_argument('--lambda_L1', type=float, default=1.0, help='weight for L1 loss')
 
         return parser
 
